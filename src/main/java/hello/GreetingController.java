6f047362255158379fb9ba98cd1d8b884a3ed419
codemachineman <tmelkioty@gmail.com>
2017-03-29 21:56:03 -0400

    thymeleaf integration

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,7 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,4 @@ public class GreetingController {
                             String.format(template, name));
     }
 	
-	@RequestMapping("/hello")
-	public String Hello(){
-		return "Hello, World!";
-	}
 }
